699d714868859997c34709b4ddfd0c549c650f8b
Subash Syangtan <subash@Mac.lan>
2025-11-22 23:07:08 -0600

    LoginMenu

--- LoginMenu.java
+++ LoginMenu.java
@@ -17,8 +17,38 @@ public class LoginMenu {
         // 2. Search Business by Type
         // 3. Saved Businesses
         // 4. Logout
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        System.out.println("======= Welcome to NeighborConnect! ======= \n");
+        System.out.println("-------Support local. Connect local.-------");
+        System.out.println("How would you like to get started?");
+        while(run) {
+            System.out.println("1. View Businesses in Location");
+            System.out.println("2. Search Businesses by Type");
+            System.out.println("3. Display Saved Businesses!");
+            System.out.println("4. Logout");
+            System.out.println("Please choose an option: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice) {
+                case 1:
+                    businessManager.viewBusinessesInLocation();
+                    break;
+                case 2:
+                    businessManager.searchBusinessByType();
+                    break;
+                case 3:
+                    currentUser.displayBusinesses();
+                    break;
+                case 4:
+                    run = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice! Please choose an option.");
+            }
+        }
     }
     
     // TODO: Add methods to handle menu selections
 }
-
